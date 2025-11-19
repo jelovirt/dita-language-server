ed05777dfb93a39c9fdb0823481db1e3a95fab75
Jarno Elovirta <jarno@elovirta.com>
2025-11-19 17:28:44 +0200

    Add test for lexer

--- src/test/java/com/elovirta/dita/xml/XmlLexerTest.java
+++ src/test/java/com/elovirta/dita/xml/XmlLexerTest.java
@@ -1,8 +1,13 @@
 package com.elovirta.dita.xml;
 
+import static com.elovirta.dita.xml.XmlLexer.TokenType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class XmlLexerTest {
@@ -12,12 +17,73 @@ public class XmlLexerTest {
   @Test
   void tokenize() {
     lexer.setInput(readResource("/serializer/src/test.xml"));
-    while (lexer.hasNext()) {
-      XmlLexer.TokenType type = lexer.next();
-      System.out.printf(
-          "%s[%d:%d] '%s'%n",
-          type, lexer.getLine(), lexer.getColumn(), new String(lexer.getText()));
-    }
+
+    var act = new ArrayList<XmlLexer.TokenType>();
+    lexer.forEachRemaining(act::add);
+
+    assertEquals(
+        List.of(
+            XML_DECL_START,
+            WHITESPACE,
+            NAME,
+            EQUALS,
+            ATTR_QUOTE,
+            ATTR_VALUE,
+            ATTR_QUOTE,
+            XML_DECL_END,
+            WHITESPACE,
+            DOCTYPE_START,
+            WHITESPACE,
+            NAME,
+            WHITESPACE,
+            NAME,
+            WHITESPACE,
+            ATTR_QUOTE,
+            ATTR_VALUE,
+            ATTR_QUOTE,
+            WHITESPACE,
+            ATTR_QUOTE,
+            ATTR_VALUE,
+            ATTR_QUOTE,
+            DOCTYPE_END,
+            WHITESPACE,
+            ELEMENT_START,
+            NAME,
+            WHITESPACE,
+            NAME,
+            EQUALS,
+            ATTR_QUOTE,
+            ATTR_VALUE,
+            ATTR_QUOTE,
+            ELEMENT_END,
+            WHITESPACE,
+            ELEMENT_START,
+            NAME,
+            WHITESPACE,
+            NAME,
+            EQUALS,
+            ATTR_QUOTE,
+            ATTR_VALUE,
+            ATTR_QUOTE,
+            WHITESPACE,
+            ELEMENT_END,
+            NAME,
+            WHITESPACE,
+            NAME,
+            ELEMENT_CLOSE,
+            NAME,
+            ELEMENT_END,
+            WHITESPACE,
+            COMMENT_START,
+            COMMENT_BODY,
+            COMMENT_END,
+            WHITESPACE,
+            ELEMENT_CLOSE,
+            NAME,
+            ELEMENT_END,
+            WHITESPACE,
+            EOF),
+        act);
   }
 
   private String readResource(String name) {
