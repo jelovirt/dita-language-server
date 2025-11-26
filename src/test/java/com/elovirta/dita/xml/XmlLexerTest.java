bd27ee2bea2df7b44a459bf17c8f73ace90f2c1c
Jarno Elovirta <jarno@elovirta.com>
2025-11-26 16:24:28 +0200

    Add XML lexer test

--- src/test/java/com/elovirta/dita/xml/XmlLexerTest.java
+++ src/test/java/com/elovirta/dita/xml/XmlLexerTest.java
@@ -1,8 +1,10 @@
 package com.elovirta.dita.xml;
 
-import static com.elovirta.dita.xml.XmlLexer.TokenType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
@@ -13,78 +15,32 @@ import org.junit.jupiter.api.Test;
 public class XmlLexerTest {
 
   private final XmlLexer lexer = new XmlLexer();
+  Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
   @Test
   void tokenize() {
     lexer.setInput(readResource("/serializer/src/test.xml"));
 
-    var act = new ArrayList<XmlLexer.TokenType>();
-    lexer.forEachRemaining(act::add);
+    var act = new ArrayList<Event>();
+    lexer.forEachRemaining(
+        e ->
+            act.add(
+                new Event(
+                    e,
+                    new String(lexer.getText()),
+                    lexer.getLine(),
+                    lexer.getColumn(),
+                    lexer.getOffset())));
 
-    assertEquals(
-        List.of(
-            XML_DECL_START,
-            WHITESPACE,
-            NAME,
-            EQUALS,
-            ATTR_QUOTE,
-            ATTR_VALUE,
-            ATTR_QUOTE,
-            XML_DECL_END,
-            WHITESPACE,
-            DOCTYPE_START,
-            WHITESPACE,
-            NAME,
-            WHITESPACE,
-            NAME,
-            WHITESPACE,
-            ATTR_QUOTE,
-            ATTR_VALUE,
-            ATTR_QUOTE,
-            WHITESPACE,
-            ATTR_QUOTE,
-            ATTR_VALUE,
-            ATTR_QUOTE,
-            DOCTYPE_END,
-            WHITESPACE,
-            ELEMENT_START,
-            NAME,
-            WHITESPACE,
-            NAME,
-            EQUALS,
-            ATTR_QUOTE,
-            ATTR_VALUE,
-            ATTR_QUOTE,
-            ELEMENT_END,
-            WHITESPACE,
-            ELEMENT_START,
-            NAME,
-            WHITESPACE,
-            NAME,
-            EQUALS,
-            ATTR_QUOTE,
-            ATTR_VALUE,
-            ATTR_QUOTE,
-            WHITESPACE,
-            ELEMENT_END,
-            NAME,
-            WHITESPACE,
-            NAME,
-            ELEMENT_CLOSE,
-            NAME,
-            ELEMENT_END,
-            WHITESPACE,
-            COMMENT_START,
-            COMMENT_BODY,
-            COMMENT_END,
-            WHITESPACE,
-            ELEMENT_CLOSE,
-            NAME,
-            ELEMENT_END,
-            WHITESPACE,
-            EOF),
-        act);
+    var exp =
+        gson.fromJson(
+            readResource("/serializer/exp/test.xml.json"),
+            new TypeToken<List<Event>>() {}.getType());
+
+    assertEquals(exp, act);
   }
+
+  private record Event(XmlLexer.TokenType type, String text, int line, int column, int offset) {}
 
   private String readResource(String name) {
     try (InputStream in = getClass().getResourceAsStream(name)) {
