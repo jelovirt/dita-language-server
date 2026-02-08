49b40a5efd772addf4b44105284bfa2e57031952
Jarno Elovirta <jarno@elovirta.com>
2026-02-08 21:20:09 +0200

    Refactor if to switch

--- src/main/java/com/elovirta/dita/xml/XmlFilter.java
+++ src/main/java/com/elovirta/dita/xml/XmlFilter.java
@@ -15,75 +15,79 @@ public class XmlFilter extends AbstractXmlFilter {
 
   @Override
   void filter() {
-    if (getType() == XmlLexerImpl.TokenType.EQUALS) {
-      switch (peek()) {
-        case WHITESPACE -> {
-          clearPeek();
-        }
-        case ATTR_NAME -> {
-          pushToBuffer(XmlLexerImpl.TokenType.ATTR_QUOTE, new char[] {'"'}, -1, -1, -1);
-          pushPeekToBuffer();
-          switch (peek()) {
-            case ATTR_QUOTE -> {
-              pushPeekToBuffer();
-            }
-            default -> {
-              pushToBuffer(XmlLexerImpl.TokenType.ATTR_QUOTE, new char[] {'"'}, -1, -1, -1);
-              pushPeekToBuffer();
-            }
+    switch (getType()) {
+      case EQUALS -> {
+        switch (peek()) {
+          case WHITESPACE -> {
+            clearPeek();
           }
-        }
-        default -> {
-          pushPeekToBuffer();
-        }
-      }
-    } else if (getType() == XmlLexerImpl.TokenType.ATTR_VALUE) {
-      switch (peek()) {
-        case ATTR_QUOTE -> {
-          pushPeekToBuffer();
-        }
-        default -> {
-          pushToBuffer(XmlLexerImpl.TokenType.ATTR_QUOTE, new char[] {'"'}, -1, -1, -1);
-          pushPeekToBuffer();
-        }
-      }
-    } else if (getType() == XmlLexerImpl.TokenType.ELEMENT_CLOSE) {
-      switch (peek()) {
-        case ELEMENT_NAME_END -> {
-          var stackName = elementStack.peek();
-          if (!Arrays.equals(getPeekText(), stackName)
-              && Utils.startsWith(stackName, getPeekText())) {
-            logger.debug(
-                "Correct end tag name from {} to {}",
-                String.valueOf(getPeekText()),
-                String.valueOf(stackName));
-            setPeekText(stackName);
-          }
-          pushPeekToBuffer();
-          switch (peek()) {
-            case ELEMENT_END -> {
-              pushPeekToBuffer();
-            }
-            case WHITESPACE -> {
-              pushPeekToBuffer();
-              switch (peek()) {
-                case ELEMENT_END -> {
-                  pushPeekToBuffer();
-                }
-                default -> {
-                  pushToBuffer(TokenType.ELEMENT_END, new char[] {'>'}, -1, -1, -1);
-                  pushPeekToBuffer();
-                }
+          case ATTR_NAME -> {
+            pushToBuffer(TokenType.ATTR_QUOTE, new char[] {'"'}, -1, -1, -1);
+            pushPeekToBuffer();
+            switch (peek()) {
+              case ATTR_QUOTE -> {
+                pushPeekToBuffer();
+              }
+              default -> {
+                pushToBuffer(TokenType.ATTR_QUOTE, new char[] {'"'}, -1, -1, -1);
+                pushPeekToBuffer();
               }
             }
-            default -> {
-              pushToBuffer(TokenType.ELEMENT_END, new char[] {'>'}, -1, -1, -1);
-              pushPeekToBuffer();
-            }
+          }
+          default -> {
+            pushPeekToBuffer();
           }
         }
-        default -> {
-          pushPeekToBuffer();
+      }
+      case ATTR_VALUE -> {
+        switch (peek()) {
+          case ATTR_QUOTE -> {
+            pushPeekToBuffer();
+          }
+          default -> {
+            pushToBuffer(TokenType.ATTR_QUOTE, new char[] {'"'}, -1, -1, -1);
+            pushPeekToBuffer();
+          }
+        }
+      }
+      case ELEMENT_CLOSE -> {
+        switch (peek()) {
+          case ELEMENT_NAME_END -> {
+            var stackName = elementStack.peek();
+            if (!Arrays.equals(getPeekText(), stackName)
+                && Utils.startsWith(stackName, getPeekText())) {
+              logger.debug(
+                  "Correct end tag name from {} to {}",
+                  String.valueOf(getPeekText()),
+                  String.valueOf(stackName));
+              setPeekText(stackName);
+            }
+            pushPeekToBuffer();
+            switch (peek()) {
+              case ELEMENT_END -> {
+                pushPeekToBuffer();
+              }
+              case WHITESPACE -> {
+                pushPeekToBuffer();
+                switch (peek()) {
+                  case ELEMENT_END -> {
+                    pushPeekToBuffer();
+                  }
+                  default -> {
+                    pushToBuffer(TokenType.ELEMENT_END, new char[] {'>'}, -1, -1, -1);
+                    pushPeekToBuffer();
+                  }
+                }
+              }
+              default -> {
+                pushToBuffer(TokenType.ELEMENT_END, new char[] {'>'}, -1, -1, -1);
+                pushPeekToBuffer();
+              }
+            }
+          }
+          default -> {
+            pushPeekToBuffer();
+          }
         }
       }
     }
